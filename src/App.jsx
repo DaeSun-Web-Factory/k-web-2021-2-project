6ef03e3bfaf7a57d6188c32cdb43a7fbc820f136
SongDaeSun <backgom07@gmail.com>
2021-09-25 23:28:07 +0900

    done build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 
+// import AWS
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react';
@@ -11,8 +12,15 @@ import SongList from './components/SongList'
 Amplify.configure(awsconfig);
 
 function App() {
+
     return (
         <div className="App">
+            <header className="App-header">
+                <AmplifySignOut/>
+
+                <h2>Game Sounds</h2>
+            </header>
+
             <SongList/>
 
         </div>
